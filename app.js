f4d2defb4b0d34acfd96d8755261d5710246f5af
Dario Guarascio <dario.guarascio@gmail.com>
2022-04-05 17:59:46 +0200

    WIP

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const moment = require('moment');
 moment.locale('it');
+moment.tz.setDefault('Europe/Rome');
 
 const axios = require('axios');
 const mcache = require('memory-cache');
